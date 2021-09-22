7e25715520755080496282c9c93eeb8b1d5c9baa
junhaotan <40597739+junhaotan@users.noreply.github.com>
2021-09-22 21:17:30 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Import express
+let express = require('express');
+// Import Body parser
+let bodyParser = require('body-parser');
+// Import Mongoose
+let mongoose = require('mongoose');
+// Initialise the app
+let app = express();
+
+// Import routes
+let apiRoutes = require("./api-routes");
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.vl3kf.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+// Use Api routes in the App
+app.use('/api', apiRoutes);
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Running RestHub on port " + port);
+});
