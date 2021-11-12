13778ba728aa98f14b7ab28000d364377283f170
junhaotan <40597739+junhaotan@users.noreply.github.com>
2021-11-12 19:43:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,3 +32,11 @@ app.listen(port, function () {
 });
 
 module.exports = app;
+
+exports.handler = async (event) => {
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
